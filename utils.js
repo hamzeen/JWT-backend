2cc8c2cef7156d25b04ac618b9db4f60bf3d5031
Hamzeen. H <hamzeen@gmail.com>
2020-03-11 11:00:05 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,22 +2,18 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
   validateToken: (req, res, next) => {
-    let result;
     if (req.headers.authorization) {
-      const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
-      const options = {
-        expiresIn: '2d',
-        issuer: 'https://scotch.io'
-      };
+      const extractedToken = req.headers.authorization.split(' ')[1]; // Bearer <token>
+      const options = { expiresIn: '2d', issuer: 'http://hamzeen.github.io' };
       try {
-        result = jwt.verify(token, process.env.JWT_SECRET, options);
+        const result = jwt.verify(extractedToken, process.env.JWT_SECRET, options);
         req.decoded = result;
         next();
       } catch (err) {
         throw new Error(err);
       }
     } else {
-      result = { 
+      const result = { 
         error: `Authentication error. Token required.`,
         status: 401 
       };
