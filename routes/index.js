9d61895aea0bb58b5fc9d00aa4dfdee3d506023c
Hamzeen. H <hamzeen@gmail.com>
2020-03-11 17:02:18 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,28 @@
-const users = require('./users');
+// index.js - index route module
+const controller = require('../controllers/users');
+const validateToken = require('../utils').validateToken;
 
-module.exports = (router) => {
-  users(router);
-  return router;
-};
+const express = require('express');
+const router = express.Router();
+
+// User routes
+router.route('/users')
+    .post(controller.add)
+    .get(validateToken, controller.getAll);
+
+// Login route
+router.route('/login')
+    .post(controller.login);
+
+// Home page route
+router.get('/', function(req, res) {
+    res.send('api ver 1.0.0');
+});
+
+// About page route
+router.get('/about', function(req, res) {
+    console.log('log:: about');
+    res.send('About');
+});
+
+module.exports = router;
